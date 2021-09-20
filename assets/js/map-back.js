4d63c2f437a05397ad956c2081ae24506fb4851c
Fecox <89794563+Fecox@users.noreply.github.com>
2021-09-20 17:25:01 -0300

    pruebas

--- assets/js/map-back.js
+++ assets/js/map-back.js
@@ -79,7 +79,9 @@ form.addEventListener('submit', (e) =>{
         fetch('/api', {
             method: 'POST',
             body: fomrdata
-        })
+        }).then(setTimeout(function(){
+            location.reload()
+        },2000))
     })
 })
 
