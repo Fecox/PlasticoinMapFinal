65a5c28dcda2d44d6f2b6be69e6eedcaaf59df33
Fecox <89794563+Fecox@users.noreply.github.com>
2021-10-13 19:13:26 -0300

    edit/delete

--- assets/js/map-back.js
+++ assets/js/map-back.js
@@ -1,5 +1,6 @@
 // set up of map
 
+
 let myMap = L.map('myMap').setView([-34.9112377243155, -54.96569965207551], 15)
 
 // tile and copyright declaration
@@ -22,14 +23,29 @@ var tempLayer = new L.FeatureGroup();
 var dblclickEvent = false;
 // var detect;
 
+const buttonB = document.getElementById('edit_div');
+const bottomM = document.getElementById('bottom_menu');
 const imagediv = document.getElementById('div__image');
 const form = document.getElementById('form')
 const formAnm = document.getElementById('div_form')
+const modalmenu = document.getElementById('modal_menu');
+const nameMarker = document.getElementById('name_marker');
+const tipoMaker = document.getElementById('Tipo_marker');
+const imgMaker = document.getElementById('img_marker');
+const nameEdit = document.getElementById('name_edit');
+const popEdit = document.getElementById('popUp_edit');
+const imgEdit = document.getElementById('img_edit');
+const tipoEdit = document.getElementById('tipo_edit');
+const colorEdit = document.getElementById('edit__color');
 const formString = "form"
+const bottomString = "edit__menu edit__button_move"
 var latLong
+var isOpen = false
+var markersData;
+var markerRes;
+
 
 // crop image and submit form
-const croppImg = document.getElementById('preview')
 var image = document.getElementById('image');
 var cropper,reader,file,canvas;
 
@@ -79,7 +95,6 @@ form.addEventListener('submit', (e) =>{
     fomrdata.set('popUpinfo', pop)
  
     
-    
     canvas = cropper.getCroppedCanvas({
         height: 300,
         width: 300
@@ -98,6 +113,18 @@ form.addEventListener('submit', (e) =>{
 function closeCrop(){
     imagediv.classList.toggle("div__image_hide");
 }
+function bottomMenu(){
+    var formHide = formAnm.classList.value;
+    if (formHide !== formString) {
+        tempLayer.clearLayers()
+        formAnm.classList.toggle("form__move")
+        buttonB.classList.toggle("edit__div_hide");
+        bottomM.classList.toggle("edit__button_move");   
+    }else{
+        buttonB.classList.toggle("edit__div_hide");
+        bottomM.classList.toggle("edit__button_move");   
+    }
+}
 
 // doble click event
 
@@ -113,7 +140,14 @@ myMap.on('dblclick', (e) =>{
     // aadadadad
     var formHide = formAnm.classList.value;
     if(formHide === formString){
-        formAnm.classList.toggle("form__move")
+        var editHide = bottomM.classList.value;
+        if (editHide === bottomString) {
+            formAnm.classList.toggle("form__move");
+            buttonB.classList.toggle("edit__div_hide");
+            bottomM.classList.toggle("edit__button_move");  
+        }else{
+            formAnm.classList.toggle("form__move");
+        }
     }
 })
 
@@ -142,7 +176,7 @@ async function getEmpresa() {
         
         let iconMarker = L.icon({
             iconUrl: data[i].icon_url,
-            shadowUrl: '/frames/empresa.png',
+            shadowUrl: '/frames/Empresa Adheridas.png',
 
             shadowSize: [46, 46],
             shadowAnchor: [23, 23],
@@ -151,7 +185,8 @@ async function getEmpresa() {
         })
     
        var marker = L.marker([data[i].lat, data[i].lon,], { icon: iconMarker})
-       marker.bindPopup(`${data[i].popUpinfo}`);
+       marker.on('click', onClick);
+       marker.bindPopup(`${data[i].popUpinfo}`);       
        Empresa.addLayer(marker);
     }
     return false;
@@ -168,7 +203,7 @@ async function getAcopio() {
         
         let iconMarker = L.icon({
             iconUrl: data[i].icon_url,
-            shadowUrl: '/frames/acopio.png',
+            shadowUrl: '/frames/Centros de Acopio.png',
 
             shadowSize: [46, 46],
             shadowAnchor: [23, 23],
@@ -177,6 +212,7 @@ async function getAcopio() {
         })
     
        var marker = L.marker([data[i].lat, data[i].lon,], { icon: iconMarker})
+       marker.on('click', onClick);
        marker.bindPopup(`${data[i].popUpinfo}`);
        Acopio.addLayer(marker);
     }
@@ -195,7 +231,7 @@ async function getBeneficio() {
         
         let iconMarker = L.icon({
             iconUrl: data[i].icon_url,
-            shadowUrl: '/frames/beneficio.png',
+            shadowUrl: '/frames/Empresas Responsables.png',
 
             shadowSize: [46, 46],
             shadowAnchor: [23, 23],
@@ -204,10 +240,111 @@ async function getBeneficio() {
         })
     
        var marker = L.marker([data[i].lat, data[i].lon,], { icon: iconMarker})
+       marker.on('click', onClick);
        marker.bindPopup(`${data[i].popUpinfo}`);
        Beneficio.addLayer(marker);
     }
     return false;
+}
+
+
+// onclick in marker
+function onClick(e){
+    var formHide = formAnm.classList.value;
+    if (!isOpen) {
+        if (formHide !== formString) {
+            tempLayer.clearLayers()
+            formAnm.classList.toggle("form__move")
+            buttonB.classList.toggle("edit__div_hide");    
+        }else{
+            buttonB.classList.toggle("edit__div_hide");
+        }
+        isOpen = true;
+        getmarkers(e);
+    }else{
+        getmarkers(e);
+    }
+}
+
+// find marker in database
+function getmarkers(info){
+    var tipo = info.sourceTarget._shadow.src;
+    tipo = tipo.replace("http://localhost:3000/frames/", '');
+    tipo = tipo.replace(".png", '');
+    tipo = tipo.replace(/%20/g, " ");
+    var lat = info.latlng.lat;
+    var lon = info.latlng.lng;
+    var data = {lat: `${lat}`, lon: `${lon}`, tipo: `${tipo}`}
+    
+    
+    fetch('/markers', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json())
+    .catch(error => console.error('error:', error))
+    .then(response => markerRes = response);
+}
+
+// confirm delete marker
+function confirmDelete(){
+    modalmenu.classList.toggle("modal__menu__anim");
+    nameMarker.innerHTML = markerRes[0].name;
+    tipoMaker.innerHTML = markerRes[0].Tipo;
+    imgMaker.src=`${markerRes[0].icon_url}`;
+}
+
+// edit marker
+
+
+// delete marker 
+function deleteMarker(){ 
+
+    fetch('/delete', {
+        method: 'POST',
+        body: JSON.stringify(markerRes),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    }).then(console.log("marker eliminado"))
+    .catch(error => console.error('error:', error))
+    .then(setTimeout(function(){
+        location.reload()
+    },2000))
+}
+// modify marker
+function modifyButton(){
+    var formHide = formAnm.classList.value;
+    if (formHide !== formString) {
+        tempLayer.clearLayers()
+        formAnm.classList.toggle("form__move")
+        buttonB.classList.toggle("edit__div_hide");
+        bottomM.classList.toggle("edit__button_move");   
+    }else{
+        buttonB.classList.toggle("edit__div_hide");
+        bottomM.classList.toggle("edit__button_move");   
+    }
+    nameEdit.value = markerRes[0].name;
+    var pop = markerRes[0].popUpinfo;
+    pop = pop.replace(/<br>/g, "\r\n");
+    popEdit.value = pop;
+    imgEdit.src = markerRes[0].icon_url;
+    tipoEdit.innerHTML = markerRes[0].Tipo;
+    switch (markerRes[0].Tipo){
+        case 'Empresas Responsables':
+            colorEdit.classList.toggle("checkmark__beneficio");
+            break;
+        case 'Empresa Adheridas':
+            colorEdit.classList.toggle("checkmark__empresa");
+            break;
+        case 'Centros de Acopio':
+            colorEdit.classList.toggle("checkmark__acopio");
+            break;
+        default:
+            console.log("algo salio mal");
+    }
 }
 
 // poot the grouplayer in the map
