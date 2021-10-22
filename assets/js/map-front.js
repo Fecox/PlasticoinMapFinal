a3ace4fa0e55fb0f9d94a319fc7ce5d05f48306b
Fecox <89794563+Fecox@users.noreply.github.com>
2021-10-22 00:08:36 -0300

    fix bug location

--- assets/js/map-front.js
+++ assets/js/map-front.js
@@ -140,7 +140,7 @@
                     for(o = 0; o < data.length; o++){
                         markers = Empresa.getLayers();
                         var name = markers[o]._icon.src;
-                        name = name.replace("http://18.228.184.234:3000/img/", '');
+                        name = name.replace("http://www.map.plasticoin.com.uy/img/", '');
                         name = name.replace(".png", '');
                         name = name.replace(/%20/g, " ");
                         if (name == data[i].name) {
@@ -152,7 +152,7 @@
                     for(o = 0; o < data.length; o++){
                         markers = Acopio.getLayers();
                         var name = markers[o]._icon.src;
-                        name = name.replace("http://18.228.184.234:3000/img/", '');
+                        name = name.replace("http://www.map.plasticoin.com.uy/img/", '');
                         name = name.replace(".png", '');
                         name = name.replace(/%20/g, " ");
                         if (name == data[i].name) {
@@ -164,7 +164,7 @@
                     for(o = 0; o < data.length; o++){
                         markers = Beneficio.getLayers();
                         var name = markers[o]._icon.src;
-                        name = name.replace("http://18.228.184.234:3000/img/", '');
+                        name = name.replace("http://www.map.plasticoin.com.uy/img/", '');
                         name = name.replace(".png", '');
                         name = name.replace(/%20/g, " ");
                         if (name == data[i].name) {
